50f797d81aebc3199a1b70120885a58d712596bb
Ankush Soni <ankushawsm@gmail.com>
2023-07-22 23:00:41 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,16 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1.sort()
+  str2.sort()
+  if (str1.length === str2.length) {
+    for (let i = 0; i < str1.length ; i++) {
+       if(str1[i]!=str2[i]){
+           return false
+       } 
+    }
+     return true 
+    }
 }
 
 module.exports = isAnagram;
